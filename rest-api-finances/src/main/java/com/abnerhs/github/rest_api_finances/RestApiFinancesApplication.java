511a291d00cd2fb6f41324c233583a2c3ad9e762
Abner <rick.santos123@hotmail.com>
2026-01-07 20:19:54 -0300

    rest API initialize

---
+++ rest-api-finances/src/main/java/com/abnerhs/github/rest_api_finances/RestApiFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.abnerhs.github.rest_api_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiFinancesApplication.class, args);
+	}
+
+}
---
+++ rest-api-finances/src/test/java/com/abnerhs/github/rest_api_finances/RestApiFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnerhs.github.rest_api_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
